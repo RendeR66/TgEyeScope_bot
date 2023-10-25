401e23319ea471bc921d4311a41e567e89c0333b
RendeR <93490632+RendeR66@users.noreply.github.com>
2023-10-25 18:41:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import asyncio
+import logging
+import sys
+import aiogram
+
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
+
+logging.basicConfig(level=logging.INFO) #логирование, чтобы не пропустить важные сообщения
+TOKEN = "6751317862:AAEMM7zRMt1QUVqNZOkggXj8kxsHKPlLe0A"
+dp = Dispatcher(bot=Bot) #диспетчер
+
+
+@dp.message(CommandStart())
+async def cmd_start(message: types.Message):
+    languages = ["Русский", "English"]
+
+
+# эхобот
+# @dp.message()
+# async def echo_handler(message: types.Message) -> None:
+#     try:
+#         await message.send_copy(chat_id=message.chat.id)
+#     except TypeError:
+#         await message.answer("Nice try!")
+
+
+async def main() -> None:
+    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
